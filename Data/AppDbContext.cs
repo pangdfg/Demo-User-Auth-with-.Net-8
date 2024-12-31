d6a3b43c9c3d1abb3dd0cbe71ef32fb839f93c06
poiu <ponpottboomk@gmail.com>
2024-12-31 12:58:22 +0700

    Create Database with Identity Framework

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DemoUserAuth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoUserAuth.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
